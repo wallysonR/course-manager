e0a4dc44ebe7c10a12e3bd5a8a2fcdbdcf3a5e6b
Wallyson <wallyson@aluno.fapce.edu.br>
2022-08-02 22:23:38 -0300

    feature/createStarComponent

---
+++ src/app/courses/star/star.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StarComponent } from './star.component';
+
+
+
+@NgModule({
+  declarations: [StarComponent],
+  imports: [
+    CommonModule
+  ],
+  exports:[StarComponent]
+})
+export class StarModule { }
