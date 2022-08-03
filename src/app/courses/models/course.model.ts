db7b078aad259710b3e10673cfe4612f045dfaea
Wallyson <wallyson@aluno.fapce.edu.br>
2022-08-02 22:24:06 -0300

    refactor/removeStrictMode

--- src/app/courses/models/course.model.ts
+++ src/app/courses/models/course.model.ts
@@ -1,11 +1,11 @@
 export class CourseModel {
-    id!: number;
-    name!: string;
-    imageUrl!: string;
-    price!: number;
-    code!: string;
-    duration!: number;
-    rating!:number;
-    releaseDate!: string;
+    id: number;
+    name: string;
+    imageUrl: string;
+    price: number;
+    code: string;
+    duration: number;
+    rating:number;
+    releaseDate: string;
 
 }
