1529bd48d5aa69f4cd30d3b78bf1b797b4a6ad4c
Wallyson <wallyson@aluno.fapce.edu.br>
2022-08-02 22:38:22 -0300

    feature/bindByComponents

--- src/app/courses/course-list/course-list.module.ts
+++ src/app/courses/course-list/course-list.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseListComponent } from './course-list.component';
+import { StarModule } from '../star/star.module';
 
 
 
 @NgModule({
   declarations: [CourseListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    StarModule
   ],
   exports:[CourseListComponent]
 })
